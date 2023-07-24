a4be9244cc06f0548acc7263c6c2f8b3086426c3
akibrahman <akibrahman5200@gmail.com>
2023-07-24 18:33:26 +0600

    Navigation Bar Completed

---
+++ tailwind-custome.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        main: "#B68C5A",
+        title: "#414040",
+        subtitle: "#707070",
+      },
+      fontFamily: {
+        main: ["Poppins", "sans-serif"],
+        title: ["EB Garamond", "serif"],
+      },
+    },
+  },
+};
